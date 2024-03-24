3dec1406f1e52472c7eb1d7937b313a563e0cdfa
NipunikaRanasingha1993 <nipuniranasinghe1993@gmail.com>
2024-03-24 15:52:05 +0530

    third commit

--- pages/CustomerAction.jsx
+++ pages/CustomerAction.jsx
@@ -1,5 +1,5 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { TouchableOpacity, View } from "react-native";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
 import { Text } from "react-native-paper";
 
 export default function CustomerAction({navigation}){
@@ -10,13 +10,67 @@ export default function CustomerAction({navigation}){
     }
 
     return(
-        <View>
-            <TouchableOpacity onPress={logOutAct} style={{backgroundColor:'red'}}>
-                <Text>Log Out</Text>
+        <View style={styles.container}>
+        <View style={{flex:1}}>
+            <TouchableOpacity style={styles.btn1} onPress={logOutAct}>
+                <Text style={styles.btnText}>Log Out</Text>
             </TouchableOpacity>
+        </View>
+        <View style={{flex:1}}>
+            <TouchableOpacity style={styles.btn2}>
+                <Text style={styles.btnText}>Delete Account</Text>
+            </TouchableOpacity>
+
+        </View>
+        <View style={{flex:3}}>
+            <TouchableOpacity style={styles.btn3}>
+                <Text style={styles.btnText}>Update Details</Text>
+            </TouchableOpacity>
+        </View>
         </View>
        
 
     )
     
 }
+
+const styles = StyleSheet.create({
+
+    container : {
+        flex:1,
+        justifyContent:'flex-start',
+        alignItems:'center'
+    },
+
+    btn1 : {
+        marginTop:5,
+        backgroundColor:'red',
+        width:'50%',
+        borderRadius:20,
+        padding:10,
+        justifyContent:'center',
+        alignItems:'center',
+    },
+    btn2 : {
+        backgroundColor:'red',
+        width:'50%',
+        borderRadius:20,
+        padding:10,
+        justifyContent:'center',
+        alignItems:'center',
+
+    },
+
+    btn3 : {
+        backgroundColor:'green',
+        width:'50%',
+        borderRadius:20,
+        padding:10,
+        justifyContent:'center',
+        alignItems:'center',
+    },
+
+    btnText : {
+        color:'white'
+    }
+})
